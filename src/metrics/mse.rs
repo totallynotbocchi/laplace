00b28483477d825a0efb82434c135b8c68679a48
bocchi-chan <ihavenocluelolll@outlook.com>
2026-08-21 19:23:39 +0300

    Made MSE use slices as parameters rather than vectors

--- src/metrics/mse.rs
+++ src/metrics/mse.rs
@@ -1,6 +1,6 @@
 // Mean Squared Error
 
-pub fn mse(y_pred: &Vec<f64>, y_true: &Vec<f64>) -> f64 {
+pub fn mse(y_pred: &[f64], y_true: &[f64]) -> f64 {
     if y_pred.len() != y_true.len() {
         panic!("Sizes don't match.")
     }
@@ -13,7 +13,7 @@ pub fn mse(y_pred: &Vec<f64>, y_true: &Vec<f64>) -> f64 {
     sum / y_pred.len() as f64
 }
 
-pub fn mse_grad(y_pred: &Vec<f64>, y_true: &Vec<f64>) -> Vec<f64> {
+pub fn mse_grad(y_pred: &[f64], y_true: &[f64]) -> Vec<f64> {
     if y_pred.len() != y_true.len() {
         panic!("Sizes don't match.")
     }
@@ -35,7 +35,7 @@ mod tests {
 
     #[test]
     fn same_array() {
-        let arr1 = vec![0., 1., 2.];
+        let arr1 = [0., 1., 2.];
 
         let err = mse(&arr1, &arr1);
         assert_eq!(err, 0.);
@@ -43,8 +43,8 @@ mod tests {
 
     #[test]
     fn correctness() {
-        let y_true = vec![1., 2., 3.];
-        let y_pred = vec![1., 2., 5.];
+        let y_true = [1., 2., 3.];
+        let y_pred = [1., 2., 5.];
 
         let err = mse(&y_pred, &y_true);
         assert!((err - 4. / 3.).abs() < TOLERANCE);
@@ -52,8 +52,8 @@ mod tests {
 
     #[test]
     fn negatives() {
-        let y_true = vec![-1., -2., -3.];
-        let y_pred = vec![-1., -2., -5.];
+        let y_true = [-1., -2., -3.];
+        let y_pred = [-1., -2., -5.];
 
         let err = mse(&y_pred, &y_true);
         assert!((err - 4. / 3.).abs() < TOLERANCE);
